77d172e8837a27fc72d5b75f43aa83f5c8c44b85
Yan <joravernigusya@gmail.com>
2023-04-22 19:44:21 +0300

    Добавить 2 новых файла и обновить 3 существующих
    В этом коммите было создано 3 новых файла:
    1) base_page.py. Базовая страница с функциями проверки элементов на видимость, наличие, кликабельность и т.д. В классе BasePage добавлены функции element_is_visible(), elements_are_visible(), element_is_present(), elements_are_present(), elements_is_not_visible(), elements_is_clickable() и go_to_element(). Каждая функция имеет описание, указывающее, как она работает и какие аргументы принимает.
    2) conftest.py. Добавлена фикстура для запуска браузера Chrome в автоматическом режиме и максимальном размере окна.
    3) Файлы chromedriver.exe и test_first.py перемещены в директорию HW17
    4) requirements.txt. Добавлены зависимости pytest 7.3.1, selenium 4.9.0 и webdriver-manager 3.8.6.

---
+++ DemoQA/pages/base_page.py
@@ -0,0 +1,88 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        """
+        Функция проверяет, что элемент, заданный локатором, видим на странице.
+        :param locator: локатор элемента на странцие
+        :param timeout: время ожидания, в течение которого выполняется поиск
+        элемента
+        :return: элемент, если он найден и видимый на странице
+        """
+        return wait(self.driver, timeout).until(
+            EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):
+        """
+        Функция проверяет, что все элементы, заданные локатором, видимы на
+        странице.
+        :param locator: локатор элементов на странице
+        :param timeout: время ожидания, в течение которого выполняется поиск
+        элементов
+        :return: список элементов, если они найдены и видимы на странице
+        """
+        return wait(self.driver, timeout).until(
+            EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        """
+        Функция проверяет наличие элемента на странице.
+        :param locator: локатор элемента на странице
+        :param timeout: время ожидания, в течение которого выполняется поиск
+        элементов
+        :return: элемент, если он присутствует на странице
+        """
+        return wait(self.driver, timeout).until(
+            EC.presence_of_element_located(locator))
+
+    def elements_are_present(self, locator, timeout=5):
+        """
+        Функция проверяет наличие нескольких элементов на странице
+        :param locator: локатор элемента на странице
+        :param timeout: время ожидания, в течение которого выполняется поиск
+        элементов
+        :return: cписок элементов, если они присутствуют на странице
+        """
+        return wait(self.driver, timeout).until(
+            EC.presence_of_all_elements_located(locator))
+
+    def elements_is_not_visible(self, locator, timeout=5):
+        """
+        Функция проверяет, что элемент не отображается на странице
+        :param locator: локатор элемента на странице
+        :param timeout: время ожидания, в течение которого выполняется поиск
+        элементов
+        :return: True, если элемент не отображается на странице, иначе
+        генерируется исключение
+        """
+        return wait(self.driver, timeout).until(
+            EC.invisibility_of_element_located(locator))
+
+    def elements_is_clickable(self, locator, timeout=5):
+        """
+        Функция проверяет, что элемент можно кликнуть
+        :param locator: локатор элемента на странице
+        :param timeout: время ожидания, в течение которого выполняется поиск
+        элементов
+        :return: элемент, если он кликабелен, иначе генерируется исключение
+        """
+        return wait(self.driver, timeout).until(
+            EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        """
+        Функция используется для прокрутки страницы к указанному элементу
+        :param element:  элемент, к которому нужно прокрутить страницу
+        :return: не возвращает никакого значения
+        """
+        self.driver.execute_script("argument[0].scrollIntoView;", element)
