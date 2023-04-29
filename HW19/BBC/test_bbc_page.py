bda1a75882cb97fb01ae12241fe491a137d91c2b
Yan <joravernigusya@gmail.com>
2023-04-29 21:24:52 +0300

    Переименовать тестовые методы в test_bbc_page.py

--- HW19/BBC/test_bbc_page.py
+++ HW19/BBC/test_bbc_page.py
@@ -4,37 +4,37 @@ import pytest
 
 
 class TestBBC(BBCPageLocators):
-    def test_first_element_css(self, driver):
+    def test_promo_image_element_css(self, driver):
         page = BBCBasePage(driver, 'https://www.bbc.com/news')
         page.open_page()
         element = page.element_is_visible(self.PROMO_IMAGE_CSS)
         assert element.is_displayed()
 
-    def test_first_element_xpath(self, driver):
+    def test_promo_image_element_xpath(self, driver):
         page = BBCBasePage(driver, 'https://www.bbc.com/news')
         page.open_page()
         element = page.element_is_visible(self.PROMO_IMAGE_XPATH)
         assert element.is_displayed()
 
-    def test_second_element_css(self, driver):
+    def test_home_page_block_element_css(self, driver):
         page = BBCBasePage(driver, 'https://www.bbc.com/news')
         page.open_page()
         element = page.element_is_visible(self.HOME_PAGE_BLOCK_CSS)
         assert element.is_displayed()
 
-    def test_second_element_xpath(self, driver):
+    def test_home_page_block_element_xpath(self, driver):
         page = BBCBasePage(driver, 'https://www.bbc.com/news')
         page.open_page()
         element = page.element_is_visible(self.HOME_PAGE_BLOCK_XPATH)
         assert element.is_displayed()
 
-    def test_third_element_css(self, driver):
+    def sport_block_element_css(self, driver):
         page = BBCBasePage(driver, 'https://www.bbc.com/news')
         page.open_page()
         element = page.element_is_visible(self.SPORT_BLOCK_CSS)
         assert element.is_displayed()
 
-    def test_third_element_xpath(self, driver):
+    def sport_block_element_xpath(self, driver):
         page = BBCBasePage(driver, 'https://www.bbc.com/news')
         page.open_page()
         element = page.element_is_visible(self.SPORT_BLOCK_XPATH)
