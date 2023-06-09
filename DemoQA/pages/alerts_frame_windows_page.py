fef767ffa95bc1bf667c18b35fccba09551ac0a0
Yan <joravernigusya@gmail.com>
2023-06-09 18:49:30 +0300

    Добавить автотесты для проверки оповещений на странице https://demoqa.com/alerts
    1) alerts_frame_windows_locators.py.
    Добавлены локаторы элементов на странице https://demoqa.com/alerts.
    2) alerts_frame_windows_page.py.
     Добавлен метод check_see_alert(), который получает текст из оповещения.
     Добавлен метод check_alert_appear_5_sec(), который проверяет появление оповещения через 5 секунд.
     Добавлен метод check_confirm_alert(), который проверяет оповещение с подтверждением.
     Добавлен метод check_prompt_alert(), который проверяет оповещение с запросом.
    3) alerts_frame_windows_test.py.
    Добавлен тест test_see_alert, который проверяет открытие оповещения.
    Добавлен тест test_alert_appear_5_sec, который проверяет открытие оповещения через 5 секунд.
    Добавлен тест test_confirm_alert, который проверяет открытие оповещения с подтверждением.
    Добавлен тест test_prompt_alert, который проверяет открытие оповещения с запросом.

--- DemoQA/pages/alerts_frame_windows_page.py
+++ DemoQA/pages/alerts_frame_windows_page.py
@@ -1,7 +1,11 @@
+import random
+import time
+
 import allure
+from selenium.common import UnexpectedAlertPresentException
 
 from DemoQA.locators.alerts_frame_windows_locators import \
-    BrowserWindowsPageLocators
+    BrowserWindowsPageLocators, AlertsPageLocators
 from DemoQA.pages.base_page import BasePage
 
 
@@ -22,3 +26,47 @@ class BrowserWindowsPage(BasePage, BrowserWindowsPageLocators):
         self.driver.switch_to.window(self.driver.window_handles[1])
         text_title = self.element_is_present(self.TITLE_NEW).text
         return text_title
+
+
+class AlertsPage(BasePage, AlertsPageLocators):
+
+    @allure.step('get text from alert')
+    def check_see_alert(self):
+        # Метод получает текст из оповещения.
+        self.element_is_visible(self.SEE_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        return alert_window.text
+
+    @allure.step('check alert appear after 5 sec')
+    def check_alert_appear_5_sec(self):
+        # Метод проверяет появление оповещения через 5 секунд.
+        self.element_is_visible(
+            self.APPEAR_ALERT_AFTER_5_SEC_BUTTON).click()
+        time.sleep(6)
+        try:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+        except UnexpectedAlertPresentException:
+            alert_window = self.driver.switch_to.alert
+            return alert_window.text
+
+    @allure.step('check confirm alert')
+    def check_confirm_alert(self):
+        # Метод проверяет оповещение с подтверждением.
+        self.element_is_visible(self.CONFIRM_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.accept()
+        text_result = self.element_is_present(
+            self.CONFIRM_RESULT).text
+        return text_result
+
+    @allure.step('check prompt alert')
+    def check_prompt_alert(self):
+        # Метод проверяет оповещение с запросом.
+        text = f"autotest{random.randint(0, 999)}"
+        self.element_is_visible(self.PROMPT_BOX_ALERT_BUTTON).click()
+        alert_window = self.driver.switch_to.alert
+        alert_window.send_keys(text)
+        alert_window.accept()
+        text_result = self.element_is_present(self.PROMPT_RESULT).text
+        return text, text_result
--- DemoQA/tests/alerts_frame_windows_test.py
+++ DemoQA/tests/alerts_frame_windows_test.py
@@ -1,6 +1,9 @@
 import allure
 
-from DemoQA.pages.alerts_frame_windows_page import BrowserWindowsPage
+from DemoQA.pages.alerts_frame_windows_page import (
+    BrowserWindowsPage,
+    AlertsPage
+)
 
 
 @allure.suite('Alerts, Frame & Windows')
@@ -28,3 +31,38 @@ class TestAlertsFrameWindow:
             text_result = browser_windows_page.check_opened_new_tab()
             assert text_result == 'This is a sample page', \
                 "the new window has not opened or an incorrect window has opened"
+
+    @allure.feature('Alerts Page')
+    class TestAlertsPage:
+        @allure.title('Checking the opening of an alert')
+        def test_see_alert(self, driver):
+            # Тест проверяет открытие оповещения.
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_see_alert()
+            assert alert_text == 'You clicked a button', "Alert did not show up"
+
+        @allure.title('Checking the opening of the alert after 5 seconds')
+        def test_alert_appear_5_sec(self, driver):
+            # Тест проверяет открытие оповещения через 5 секунд.
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_alert_appear_5_sec()
+            assert alert_text == 'This alert appeared after 5 seconds', \
+                "Alert did not show up"
+
+        @allure.title('Checking the opening of the alert with confirm')
+        def test_confirm_alert(self, driver):
+            # Тест проверяет открытие оповещения с подтверждением.
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            alert_text = alert_page.check_confirm_alert()
+            assert alert_text == 'You selected Ok', "Alert did not show up"
+
+        @allure.title('Checking the opening of the alert with prompt')
+        def test_prompt_alert(self, driver):
+            # Тест проверяет открытие оповещения с запросом.
+            alert_page = AlertsPage(driver, 'https://demoqa.com/alerts')
+            alert_page.open()
+            text, alert_text = alert_page.check_prompt_alert()
+            assert text in alert_text, "Alert did not show up"
