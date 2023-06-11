131f8580dfc9a4d8e68487b67f2f8df2b08f02df
Yan <joravernigusya@gmail.com>
2023-06-11 19:34:45 +0300

    Добавить автотесты для проверки фреймов на странице https://demoqa.com/frames
    1) alerts_frame_windows_locators.py.
    Добавлены локаторы элементов на странице https://demoqa.com/alerts.
    2) alerts_frame_windows_page.py.
     Добавлен метод check_frame(), который проверяет фрейм.
    3) alerts_frame_windows_test.py.
    Добавлен тест test_frames, который проверяет  страницу с фреймам.
    запросом.

--- DemoQA/locators/alerts_frame_windows_locators.py
+++ DemoQA/locators/alerts_frame_windows_locators.py
@@ -17,3 +17,10 @@ class AlertsPageLocators:
     CONFIRM_RESULT = (By.CSS_SELECTOR, 'span[id="confirmResult"]')
     PROMPT_BOX_ALERT_BUTTON = (By.CSS_SELECTOR, 'button[id="promtButton"]')
     PROMPT_RESULT = (By.CSS_SELECTOR, 'span[id="promptResult"]')
+
+
+class FramesPageLocators:
+    # Добавлены локаторы элементов на странице https://demoqa.com/frames.
+    FIRST_FRAME = (By.CSS_SELECTOR, 'iframe[id="frame1"]')
+    SECOND_FRAME = (By.CSS_SELECTOR, 'iframe[id="frame2"]')
+    TITLE_FRAME = (By.CSS_SELECTOR, 'h1[id="sampleHeading"]')
--- DemoQA/tests/alerts_frame_windows_test.py
+++ DemoQA/tests/alerts_frame_windows_test.py
@@ -2,7 +2,8 @@ import allure
 
 from DemoQA.pages.alerts_frame_windows_page import (
     BrowserWindowsPage,
-    AlertsPage
+    AlertsPage,
+    FramesPage
 )
 
 
@@ -66,3 +67,17 @@ class TestAlertsFrameWindow:
             alert_page.open()
             text, alert_text = alert_page.check_prompt_alert()
             assert text in alert_text, "Alert did not show up"
+
+    @allure.feature('Frame Page')
+    class TestFramesPage:
+        @allure.title('Check the page with frames')
+        # Тест проверяет страницу с фреймами.
+        def test_frames(self, driver):
+            frame_page = FramesPage(driver, 'https://demoqa.com/frames')
+            frame_page.open()
+            result_frame1 = frame_page.check_frame('frame1')
+            result_frame2 = frame_page.check_frame('frame2')
+            assert result_frame1 == ['This is a sample page', '500px',
+                                     '350px'], 'The frame does not exist'
+            assert result_frame2 == ['This is a sample page', '100px',
+                                     '100px'], 'The frame does not exist'
